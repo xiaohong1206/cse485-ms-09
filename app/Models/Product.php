585276298346fa350ccef2cc7502c5ff97bb7f58
xiaohong1206 <thuyhuong12062k4@gmail.com>
2026-08-03 03:09:27 +0700

    Add files via upload

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+
+    'category_id',
+
+    'sku',
+
+    'name',
+
+    'price',
+
+    'qty',
+
+    'description'
+
+    ];
+}
