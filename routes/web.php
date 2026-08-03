13eddbc662f0b273d16e1c7057161c6e4bc9d2dc
xiaohong1206 <thuyhuong12062k4@gmail.com>
2026-08-03 13:38:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ Route::get('/admin/products', [ProductController::class, 'index'])
 
 Route::get('/admin/about', [DashboardController::class, 'about'])
     ->name('admin.about');
+    // routes/web.php (demo học tập; production nên để routes/api.php + CSRF/API auth)
+use App\Http\Controllers\Api\ProductApiController;
+
+Route::get('/api/products', [ProductApiController::class, 'index']);
+Route::get('/api/products/{id}', [ProductApiController::class, 'show']);
+Route::post('/api/products', [ProductApiController::class, 'store']);
+Route::put('/api/products/{id}', [ProductApiController::class, 'update']);
+Route::delete('/api/products/{id}', [ProductApiController::class, 'destroy']);
+use App\Http\Controllers\Lab\CategoryLabController;
+Route::get('/lab/categories', [CategoryLabController::class, 'index']);
+Route::post('/lab/categories', [CategoryLabController::class, 'store']);
+Route::get('/lab/categories/{id}', [CategoryLabController::class, 'show']);
+Route::put('/lab/categories/{id}', [CategoryLabController::class, 'update']);
+Route::delete('/lab/categories/{id}', [CategoryLabController::class, 'destroy']);
+use App\Http\Controllers\Lab\ProductLabController;
+// Product Lab
+Route::get('/lab/products', [ProductLabController::class, 'index']);
+Route::post('/lab/products', [ProductLabController::class, 'store']);
+Route::get('/lab/products/{id}', [ProductLabController::class, 'show']);
+Route::put('/lab/products/{id}', [ProductLabController::class, 'update']);
+Route::delete('/lab/products/{id}', [ProductLabController::class, 'destroy']);
