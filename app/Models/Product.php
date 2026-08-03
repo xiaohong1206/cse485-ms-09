eef5e5f7f419f5e16a180db5d7f9cc1c184ab7ae
xiaohong1206 <thuyhuong12062k4@gmail.com>
2026-08-03 13:39:13 +0700

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,24 +1,23 @@
 <?php
-
+// app/Models/Product.php
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     protected $fillable = [
-
-    'category_id',
-
-    'sku',
-
-    'name',
-
-    'price',
-
-    'qty',
-
-    'description'
-
+        'category_id',
+        'sku',
+        'name',
+        'price',
+        'qty',
+        'description',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
